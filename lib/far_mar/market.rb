787f0f390fb6f3d0341e6ea9f442cc810ef89f43
Kristina Hjertberg <hjert.k@gmail.com>
2014-09-15 15:31:50 -0700

    set up market attributes

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,13 +1,24 @@
 module FarMar
   class Market
-    def initialize
-      @csv = CSV.read "./support/markets.csv"
+    @@csv = CSV.read "./support/markets.csv"
+    attr_accessor :id, :name, :address, :city, :county, :state, :zip
+
+    def initialize(array)
+      @id = array[0]
+      @name = array[1]
+      @address = array[2]
+      @city = array[3]
+      @county = array[4]
+      @state = array[5]
+      @zip = array[6]
     end
+
     def self.all
-      CSV.read "./support/markets.csv"
+      @@csv.collect { |n| Market.new(n) }
     end
+
     def self.find(id)
-      @csv.find {|m| m[0] == id}
+      @@csv.find {|m| m[0] == id}
     end
   end
 end
