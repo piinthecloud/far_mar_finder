21a6392a5ba621caa5bd23559b7afba1566b3e78
Kristina Hjertberg <hjert.k@gmail.com>
2014-09-15 15:44:48 -0700

    Sales class update for first tests

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -1,17 +1,18 @@
 module FarMar
   class Sale
-    @@csv = CSV.read "./support/vendors.csv"
-    attr_accessor :id, :name, :num_employees, :market_id
+    @@csv = CSV.read "./support/sales.csv"
+    attr_accessor :id, :amount, :purchase_time, :vendor_id, :market_id
 
     def initialize(array)
       @id = array[0]
-      @name = array[1]
-      @num_employees = array[2]
-      @market_id = array[3]
+      @amount = array[1]
+      @purchase_time = array[2]
+      @vendor_id = array[3]
+      @market_id = array[4]
     end
 
     def self.all
-      @@csv.collect { |n| Vendor.new(n) }
+      @@csv.collect { |n| Sale.new(n) }
     end
 
     def self.find(id)
