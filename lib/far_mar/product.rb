f82fc054a9a3dde0d76618c7ca7f62f2d990211c
Kristina Hjertberg <hjert.k@gmail.com>
2014-09-15 15:42:40 -0700

    updating classes to pass the tests

--- lib/far_mar/product.rb
+++ lib/far_mar/product.rb
@@ -1,4 +1,20 @@
 module FarMar
   class Product
+    @@csv = CSV.read "./support/products.csv"
+    attr_accessor :id, :name, :vendor_id
+
+    def initialize(array)
+      @id = array[0]
+      @name = array[1]
+      @vendor_id = array[2]
+    end
+
+    def self.all
+      @@csv.collect { |n| Product.new(n) }
+    end
+
+    def self.find(id)
+      @@csv.find { |m| m[0] == id }
+    end
   end
 end
--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -1,4 +1,21 @@
 module FarMar
   class Sale
+    @@csv = CSV.read "./support/vendors.csv"
+    attr_accessor :id, :name, :num_employees, :market_id
+
+    def initialize(array)
+      @id = array[0]
+      @name = array[1]
+      @num_employees = array[2]
+      @market_id = array[3]
+    end
+
+    def self.all
+      @@csv.collect { |n| Vendor.new(n) }
+    end
+
+    def self.find(id)
+      @@csv.find { |m| m[0] == id }
+    end
   end
 end
