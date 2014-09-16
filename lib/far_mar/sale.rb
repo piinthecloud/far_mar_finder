4a6e607e834e07d9b0ca307ac1986d1609b15a28
Kristina Hjertberg <hjert.k@gmail.com>
2014-09-16 11:43:28 -0700

    parsed purchase time, vendor method for sale.rb passes tests

--- lib/far_mar/sale.rb
+++ lib/far_mar/sale.rb
@@ -6,7 +6,7 @@ module FarMar
     def initialize(array)
       @id = array[0].to_i
       @amount = array[1].to_i
-      @purchase_time = array[2]
+      @purchase_time = DateTime.parse(array[2])
       @vendor_id = array[3].to_i
       @product_id = array[4].to_i
     end
@@ -17,6 +17,18 @@ module FarMar
 
     def self.find(id)
       self.all.find {|m| m.id == id.to_i}
+    end
+
+    def self.between(beginning_time, end_time)
+
+    end
+
+    def vendor
+      FarMar::Vendor.find(@vendor_id)
+    end
+
+    def product
+
     end
   end
 end
